df95139eee4da930826cb950051abbae919cb81e
yc0583267135-cell <yc0583267135@gmail.com>
2026-07-30 20:59:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -81,4 +81,5 @@ def download():
         return f"שגיאה בהורדת הסרטון מהשרת: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
